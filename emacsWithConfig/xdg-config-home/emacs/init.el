0b099b3d2addba7e537fd563b293acf58ab5c8df
Ashlynn Anderson <pea@pea.sh>
2021-03-03 18:06:20 -0500

    Create custom.el since init.el isn't mutable
    Currently this isn't saved, but the only reason it's not is
    because (temporarily) we're setting user-emacs-directory to /tmp/emacs

--- emacsWithConfig/xdg-config-home/emacs/init.el
+++ emacsWithConfig/xdg-config-home/emacs/init.el
@@ -1,4 +1,12 @@
 ;; -*- lexical-binding: t -*-
 
+;; init.el isn't mutable, so this is necessary
+;;
+;; we set user-emacs-directory in `early-init.el` so it's safe to use
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; load our actual config
 (load-library (getenv "NIX_EMACS_INIT_PACKAGE"))
 (setenv "NIX_EMACS_INIT_PACKAGE")
