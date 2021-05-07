f8b7df315b625ab5cd9042488d23ffdae1ffe517
Ashlynn Anderson <pea@pea.sh>
2021-05-06 20:30:16 -0400

    Move `user-emacs-directory` to `~/.local/share/emacs`
    Since this is no longer config and is now state instead, it makes
    sense to use `~.local/share/` IMO

--- emacsWithConfig/xdg-config-home/emacs/early-init.el
+++ emacsWithConfig/xdg-config-home/emacs/early-init.el
@@ -5,9 +5,9 @@
 (setenv "NIX_STORED_XDG_CONFIG_HOME")
 
 ;; Set user-emacs-directory to a mutable location
-;; TODO: move this out of /tmp
-(mkdir "/tmp/emacs" t)
-(setq user-emacs-directory "/tmp/emacs") 
+(let ((emacs-directory (expand-file-name "~/.local/share/emacs")))
+  (mkdir emacs-directory t)
+  (setq user-emacs-directory emacs-directory))
 
 ;; load autoloads
 (dolist (dir load-path)
