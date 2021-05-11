b58526d8aa9f281ab9cb124590a13c094e11c5a8
Ashlynn Anderson <pea@pea.sh>
2021-05-11 00:54:04 -0400

    Fix magit-status keybind

--- config/magit/magit.el
+++ config/magit/magit.el
@@ -2,7 +2,6 @@
 
 ;; magit-status instead of magit so it takes over the current magit
 ;; window rather than opening a new one. I prefer this behavior
-(with-eval-after-load 'magit
-  (define-key global-map (kbd "C-x g") 'magit-status))
+(define-key global-map (kbd "C-x g") 'magit-status)
 
 (provide 'config-magit)
