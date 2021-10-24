52896c7f0755831d1cb4e91d322612a26036efb2
Ashlynn Anderson <lambdadog@tsunomaki.local>
2021-10-24 19:31:32 -0400

    remove input fonts

--- config/quick-ui/quick-ui.el
+++ config/quick-ui/quick-ui.el
@@ -8,18 +8,18 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
-(set-face-attribute 'default nil
-		    :font "Input Mono"
-		    :weight 'light
-		    :height 105)
+;;(set-face-attribute 'default nil
+;;		    :font "Input Mono"
+;;		    :weight 'light
+;;		    :height 105)
+;;
+;;(set-face-attribute 'variable-pitch nil
+;;		    :font "Input Serif"
+;;		    :weight 'light
+;;		    :height 105)
 
-(set-face-attribute 'variable-pitch nil
-		    :font "Input Serif"
-		    :weight 'light
-		    :height 105)
-
-;(add-hook 'prog-mode-hook
-;	  #'variable-pitch-mode)
+;;(add-hook 'prog-mode-hook
+;;	  #'variable-pitch-mode)
 
 (setq inhibit-startup-screen t)
 
