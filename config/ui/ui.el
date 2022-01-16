c289bb74c2ec3eae6db4025a7bf19617416662bb
lambdadog <github@pea.sh>
2022-01-15 20:22:49 -0500

    Add some QoL selectrum config

--- config/ui/ui.el
+++ config/ui/ui.el
@@ -24,6 +24,30 @@
 	       mode-line-format nil
 	       truncate-lines t)))
 
+  ;; Auto-refocus minibuffer on clicking the display buffer
+  ;;
+  ;; Also incidentally fixes https://github.com/raxod502/selectrum/issues/585
+  (defun config-ui/selectrum-refocus-minibuffer ()
+    (when (string= (buffer-name (window-buffer (selected-window))) selectrum--display-action-buffer)
+      (select-window (active-minibuffer-window) nil)))
+  (add-hook 'buffer-list-update-hook #'config-ui/selectrum-refocus-minibuffer)
+
+  ;; On insert, set candidate to -1, easing selection of directories
+  (defvar-local config-ui/selectrum--just-inserted-candidate nil)
+  (defun config-ui/selectrum-smart-insert (&rest _)
+    (setq-local config-ui/selectrum--just-inserted-candidate 't))
+  (defun config-ui/selectrum--compute-current-candidate-index/:filter-return (result)
+    (if (not (eq result 0))
+	result
+      (cond
+       (config-ui/selectrum--just-inserted-candidate
+	(setq-local config-ui/selectrum--just-inserted-candidate nil)
+	-1)
+       (t 0))))
+  (add-hook 'selectrum-candidate-inserted-hook #'config-ui/selectrum-smart-insert)
+  (advice-add #'selectrum--compute-current-candidate-index :filter-return
+	      #'config-ui/selectrum--compute-current-candidate-index/:filter-return)
+
   (setq
    selectrum-fix-vertical-window-height 8
    marginalia-margin-threshold 170
